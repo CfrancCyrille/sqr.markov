500aca8a48c17a8bb01df6fefa82d06c02ec2d5d
Thibaud MORIN <thibaud_morin@icloud.com>
2020-12-15 17:23:03 +0100

    Implémentation tests MarkovData

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -7,8 +7,21 @@ import org.junit.Test;
 public class MarkovDataTest {
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRead() {
+		MarkovData nbMot = new MarkovData();
+		nbMot.read("Il y a cinq mot");
+		int actual = nbMot.keyWordSize();
+		int expected = 5;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testRead8() {
+		MarkovData nbMot = new MarkovData();
+		nbMot.read("Il y a pas du tout cinq mot");
+		int actual = nbMot.keyWordSize();
+		int expected = 8;
+		assertEquals(expected, actual);
 	}
 
 }
