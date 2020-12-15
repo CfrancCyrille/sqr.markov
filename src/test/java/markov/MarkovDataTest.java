2a64eb8222e862c9474c31dd9e29b3608b6b6b78
vincent <vincent@192.168.1.16>
2020-12-15 17:40:22 +0100

    Data test

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -6,9 +6,67 @@ import org.junit.Test;
 
 public class MarkovDataTest {
 	
-	//@Test
-	public void test() {
-		fail("Not yet implemented");
+	String SimpleSentence = "Il fait beau.";
+	String ComplexSentence = "Il fait beau aujourd'hui mais demain il pleuvra.";
+
+	@Test
+	public void read_SimpleSentence_3Words() {
+		MarkovData markovData = new MarkovData();
+		
+		markovData.read(SimpleSentence);
+		int expected = 3;
+		int actual = markovData.keyWordSize();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void read_ComplexSentence_8Words() {
+		MarkovData markovData = new MarkovData();
+		
+		markovData.read(ComplexSentence);
+		int expected = 8;
+		int actual = markovData.keyWordSize();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void read_EmptySentence_0Words() {
+		MarkovData markovData = new MarkovData();
+		
+		markovData.read(" ");
+		int expected = 0;
+		int actual = markovData.keyWordSize();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void renforceWord_NewKey_False() {
+		MarkovData markovData = new MarkovData();
+		
+		boolean expected = false;
+		
+		String keyString = "Il fait";
+		String wordToLearn = "beau";
+		boolean actual = markovData.renforceWord(keyString, wordToLearn);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void renforceWord_OldKey_True() {
+		MarkovData markovData = new MarkovData();
+		
+		boolean expected = true;
+		
+		String keyString = "Il fait";
+		String wordToLearn = "beau";
+		markovData.learnWord(keyString, wordToLearn);
+		boolean actual = markovData.renforceWord(keyString, wordToLearn);
+		
+		assertEquals(expected, actual);
 	}
 
 }
