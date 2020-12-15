2a6e1228a9e338f74d8a779192271cba163b7a36
Sam MAROUN <maroun.sam@gmail.com>
2020-12-15 17:08:55 +0100

    Update MarkovDataTest
    Adding Two unit test for MarkovDataTest

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -7,8 +7,36 @@ import org.junit.Test;
 public class MarkovDataTest {
 	
 	//@Test
-	public void test() {
+	/*public void test() {
 		fail("Not yet implemented");
+	}*/
+
+	@Test
+	/**
+	 * GIVEN read
+	 * WHEN Simple sentence
+	 * THEN true
+	 */
+	public void testReadSimpleSentenceTrue(){
+		MarkovData data = new MarkovData();
+		String sentence = "Simple Sentence";
+		data.read(sentence);
+		assertEquals(2, data.keyWordSize());
+		
 	}
 
+	@Test
+	/**
+	 * GIVEN renforcedWord
+	 * WHEN Simple Sentence
+	 * THEN true
+	 */
+	public void testRenforcedWordTrue(){
+		MarkovData data = new MarkovData();
+		boolean result = true;
+		String sentence = "Simple Sentence";
+		data.learnWord(sentence, "Simple" );
+		result = data.renforceWord(sentence, "simple");
+		assertTrue(result);
+	}
 }
