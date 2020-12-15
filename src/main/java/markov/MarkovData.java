ce47ac982755f87c39eeb2f1c4268f58149c48bb
ntp95 <nitcheutchami@yahoo.fr>
2020-12-15 18:29:47 +0100

    commentaires

--- src/main/java/markov/MarkovData.java
+++ src/main/java/markov/MarkovData.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class MarkovData {
 	private Map<String, String> markovTable = new HashMap<String,String>();
 	private List<String> textwords = new ArrayList<String>();
