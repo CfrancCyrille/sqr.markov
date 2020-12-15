d9e16e0225f3c7fe7c370b6227fad108ddd221d4
cblard <56992562+cblard@users.noreply.github.com>
2020-12-15 16:51:16 +0100

    BLARD Christopher - Ajout de commentaires
    Correction des /* en /**

--- src/main/java/app/MarkovApp.java
+++ src/main/java/app/MarkovApp.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import markov.MarkovChain;
 
 /**
- * Commentaire 1
+ * Commentaire 1 
  */
 public class MarkovApp {
 	public static void main(String[] args) throws Exception {
