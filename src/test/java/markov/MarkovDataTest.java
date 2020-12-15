484320c68bb2cc60d89a5135de7a7df285892226
Lucas MASSOT <zeus@DESKTOP-AD5J5F6.localdomain>
2020-12-15 16:59:09 +0100

    ajout de test sur DATA

--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -6,9 +6,25 @@ import org.junit.Test;
 
 public class MarkovDataTest {
 	
+	// public void read(String text)
+	// public boolean renforceWord(String keyString, String wordToLearn)
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testread() {
+		MarkovData test = new MarkovData();
+		test.read("Cesont 3 mots");
+		int actual = test.keyWordSize();
+		int expected = 3;
+		assertEquals(expected, actual);	
+	}
+
+	@Test
+	public void testrenforceWord(){
+		MarkovData test = new MarkovData();
+		String laphrase = "ajout de un mots en plus";
+		test.learnWord(laphrase, "plus");
+		boolean result = test.renforceWord(laphrase, "plus");
+		assertEquals(result, true);
 	}
 
 }
