3ac7151a2335ecbd27a235e8de0fbb76fd775b06
ntp95 <nitcheutchami@yahoo.fr>
2021-01-03 23:16:45 +0100

    commentaires

--- src/main/java/markov/MarkovData.java
+++ src/main/java/markov/MarkovData.java
@@ -22,7 +22,7 @@ public class MarkovData {
 	 * @param wordToLearn
 	 * @return
 	 */
-	public boolean nnrenforceWord(String keyString, String wordToLearn) {
+	public boolean mrenforceWord(String keyString, String wordToLearn) {
 		String value;
 		boolean isNewKey = containsKeyWord(keyString);
 		
